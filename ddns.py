77058a1799fe00afd48016100383ef952413fd11
MinchinWeb <w_minchin@hotmail.com>
2023-07-22 10:25:45 -0600

    Better deal with no internet
    Closes #33

--- ddns.py
+++ ddns.py
@@ -20,6 +20,7 @@ import sys
 from time import sleep
 
 import requests
+from urllib3.exceptions import NameResolutionError
 
 DEFAULT_CACHE_FILE = ".public_ip.txt"
 DEFAULT_DDNS_UPDATE_URL = "http://domain-dns.com/ip.cgi"
@@ -32,7 +33,15 @@ logging.basicConfig(
 )
 
 
+class ConfigurationError(RuntimeError):
+    pass
+
+
 def read_public_ip(cache_file=None):
+    """
+    Queries a public website to determine public IP. Also write the received IP
+    to the cache file (on disk).
+    """
     ip = requests.get("https://api.ipify.org").text
     info("public ip address of %s" % ip)
     if cache_file is None:
@@ -57,26 +66,36 @@ def read_cached_public_ip(cache_file=None):
         return None
 
 
+def delete_cached_public_ip(cache_file=None):
+    if cache_file is None:
+        cache_file = getenv("DDNS_CACHE_FILE", DEFAULT_CACHE_FILE)
+        cache_file = (Path.cwd() / cache_file).resolve()
+
+    cache_file.unlink()
+    debug("cache file deleted!")
+
+
 def update_public_ip(base_url=None, domain_name=None, key=None, ip=None):
     if base_url is None:
         base_url = getenv("DDNS_UPDATE_URL")
     if base_url is None:
+        base_url = DEFAULT_DDNS_UPDATE_URL
+    if base_url is None:
+        # should never reach here...
         warning("No base url provided. Set 'DDNS_UPDATE_URL' environmental variable")
-        sys.exit(1)
+        raise ConfigurationError("DDNS_UPDATE_URL")
 
     if domain_name is None:
         domain_name = getenv("DDNS_DOMAIN_NAME")
     if domain_name is None:
-        warning(
-            "No domain name provided. Set 'DDNS_DOMAIN_NAME' environmental variable."
-        )
-        sys.exit(1)
+        error("No domain name provided. Set 'DDNS_DOMAIN_NAME' environmental variable.")
+        raise ConfigurationError("DDNS_DOMAIN_NAME")
 
     if key is None:
         key = getenv("DDNS_KEY")
     if key is None:
-        warning("No API key provided. Set 'DDNS_KEY' environmental variable.")
-        sys.exit(1)
+        error("No API key provided. Set 'DDNS_KEY' environmental variable.")
+        raise ConfigurationError("DDNS_KEY")
 
     payload = {
         "host": domain_name,
@@ -95,10 +114,14 @@ def update_public_ip(base_url=None, domain_name=None, key=None, ip=None):
     else:
         warning("Unsuccessful in updating public ip address for %s" % domain_name)
         warning("Return code %s" % r.status_code)
+        raise RuntimeError
         return
 
 
 def check_config():
+    """
+    Checks configuration, to make sure everything is in place.
+    """
     debug("Configuration is:")
     cache_file = getenv("DDNS_CACHE_FILE")
     if cache_file:
@@ -137,18 +160,29 @@ def main_loop(endless=False, cache_file=None, sleep_sec=5 * 60):
 
     while True:
         former_ip = read_cached_public_ip(cache_file=cache_file)
-        public_ip = read_public_ip(cache_file=cache_file)
-
-        if public_ip != former_ip:
-            update_public_ip()
+        try:
+            public_ip = read_public_ip(cache_file=cache_file)
+        except NameResolutionError:
+            warning(
+                "Unable to resolve domain name (to read public IP). "
+                "Do you have a working internet connection?"
+            )
+            delete_cached_public_ip()
         else:
-            info("Public IP unchanged, not updating...")
+            if public_ip != former_ip:
+                try:
+                    update_public_ip()
+                except (ConfigurationError, NameResolutionError, RuntimeError) as e:
+                    # error messages displayed when raised above
+                    delete_cached_public_ip()
+            else:
+                info("Public IP unchanged, not updating...")
 
-        if endless is False:
-            info("Exiting program...")
-            break
+            if endless is False:
+                info("Exiting program...")
+                break
 
-        sleep_time = random.randrange(sleep_sec * 0.9, sleep_sec * 1.1)
+        sleep_time = random.randrange(sleep_sec * 0.8, sleep_sec * 1.2)
         info("Sleeping for %s seconds" % sleep_time)
         sleep(sleep_time)
 
